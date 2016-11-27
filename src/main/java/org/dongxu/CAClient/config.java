79eadf8ee7211319693491b43fef2f1235a17f57
Your Name <you@example.com>
2016-11-27 01:21:58 -0800

    Removed redundant garbage files

--- src/main/java/org/dongxu/CAClient/config.java
+++
@@ -1,8 +0,0 @@
-package org.dongxu.CAClient;
-
-/**
- * Created by dongxu on 11/20/16.
- */
-public class config {
-    public static final String REST_BASE_URL = "http://localhost:8080/api/rest";
-}
--- src/main/java/org/dongxu/rs/client/Client.java
+++
@@ -1,8 +0,0 @@
-package org.dongxu.rs.client;/**
- * Created by joaorocha on 22/07/14.
- */
-public class Client {
-  public static void main(String[] argv) {
-      System.out.println("Cliente iniciado");
-  }
-}

52068646239585f6aa70b75e7b3dfc1c60321271
Your Name <you@example.com>
2016-12-04 15:25:19 -0800

    Setup client

--- Clients/src/main/java/org/dongxu/CAClient/config.java
+++ Clients/src/main/java/org/dongxu/CAClient/config.java
@@ -4,5 +4,6 @@ package org.dongxu.CAClient;
  * Created by dongxu on 11/20/16.
  */
 public class config {
-    public static final String REST_BASE_URL = "http://localhost:8080/api/rest";
+    //public static final String REST_BASE_URL = "http://localhost:8080/api/rest";
+    public static final String REST_BASE_URL = "http://35.161.220.201:8080/api/rest";
 }
